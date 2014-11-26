b732ed4807b902abf02868b4b89cc9cd28586289
andrewphillipdoss <andrewphillpdoss@gmail.com>
2014-11-25 21:02:26 -0500

    Create connect.php

---
+++ SH_PHP/connect.php
@@ -0,0 +1,21 @@
+<?php
+
+$user = 'root';
+$password = 'root';
+$db = 'SouthernHospitality';
+$host = 'localhost';
+$port = 3306;
+
+$link = mysqli_init();
+$success = mysqli_real_connect(
+   $link, 
+   $host, 
+   $user, 
+   $password, 
+   $db,
+   $port
+);
+
+echo "yah, bitch, yah";
+
+?>
